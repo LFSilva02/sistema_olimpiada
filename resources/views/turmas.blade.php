86abe464d9f38ecb97e145c7e63388faf6b99597
LfSilva02 <luizfelipe.silva@edu.unifil.br>
2024-10-24 16:32:09 -0300

    retirada de link desnessario

--- resources/views/turmas.blade.php
+++ resources/views/turmas.blade.php
@@ -7,7 +7,6 @@
   <!-- Adicionando o Tailwind CSS -->
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.0/font/bootstrap-icons.min.css">
-  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
   <style>
     .sidebar-hidden {
       transform: translateX(-100%);
